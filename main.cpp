335d839f6adc26ca89f7f407f2b555013abd500f
anwardwk <113156321+anwardwk@users.noreply.github.com>
2022-09-20 23:08:46 +0700

    Tugas Daspro ke 2

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main() {
+/*
+    Nama : Anwar Dwiky Fitriyanto
+    NIM : A11.2022.14611
+
+*/
+    int u;
+    string n = "Anwar_Dwiky_F";
+    float t = 170;
+    char goldar = 'A';
+    string ub = "L";
+    cout << "Berapa umur anda :";
+    cin >> u;
+
+    cout << "Nama Anda Adalah : " << endl;
+    cin >> n;
+    cout << "Hai Nama Saya  " << n << endl;
+
+    cout << "Umur " << u << endl;
+
+
+    cout << "Ukuran Baju Saya " << ub << endl;
+    cout << "Tinggi saya : " << t << " cm" << endl;
+    cout << "Golongan darah saya " << goldar << endl;
+    cout << "Sekian Dan Terimakasih" << endl;
+
+
+}
